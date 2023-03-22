f0f2548fe57f05f170ebbab6887dfbe6c86a4787
David Junior <david.lourenco94@gmail.com>
2023-03-22 17:52:35 -0300

    feat: test_encrypt.py

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,16 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+  
+    assert encrypt_message("abcde", 2) == "bacde_edcba"
+    assert encrypt_message("1234567", 3) == "321_7654"
+
+    assert encrypt_message("abcde", 6) == "edcba"
+
+    with pytest.raises(TypeError):
+        encrypt_message("abcde", "2")
+
+    with pytest.raises(TypeError):
+        encrypt_message(12345, 2)
