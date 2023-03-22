a1a71db7778a5491511d50c149f9b376be116cbd
David Junior <david.lourenco94@gmail.com>
2023-03-22 18:22:14 -0300

    fix: test_encrypt.py

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -2,15 +2,20 @@ from challenges.challenge_encrypt_message import encrypt_message
 import pytest
 
 
-def test_encrypt_message():
-  
-    assert encrypt_message("abcde", 2) == "bacde_edcba"
-    assert encrypt_message("1234567", 3) == "321_7654"
+def test_encrypt_message(): 
+    # Testando a função com valores válidos
+    assert encrypt_message("flamengo", 3) == "alf_ognem"
+    assert encrypt_message("barcelona", 4) == "anole_crab"
 
-    assert encrypt_message("abcde", 6) == "edcba"
-
+    # Testando a função com uma chave inválida
     with pytest.raises(TypeError):
-        encrypt_message("abcde", "2")
+        encrypt_message("hello world", "invalid_key")
 
+    # Testando a função com uma mensagem inválida
     with pytest.raises(TypeError):
-        encrypt_message(12345, 2)
+        encrypt_message(12345, 5)
+
+    # Testando a função com uma chave fora do intervalo válido
+    assert encrypt_message("hello world", 0) == "dlrow olleh"
+    assert encrypt_message("hello world", 50) == "dlrow olleh"
+
