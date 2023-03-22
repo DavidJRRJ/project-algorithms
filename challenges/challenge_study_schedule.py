584fb991e827848068d01229b68ec9278e7a1ae9
David Junior <david.lourenco94@gmail.com>
2023-03-22 17:29:08 -0300

    feat: function study_schedule req01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,12 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if target_time is None:
+        return None
+
+    count = 0
+    for entry, exit in permanence_period:
+        if not all(isinstance(time, int) for time in (entry, exit)):
+            return None
+        if entry <= target_time <= exit:
+            count += 1
+
+    return count
